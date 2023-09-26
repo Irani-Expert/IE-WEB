b5de3d749d203059e42b5e2995b9d0e5f066588a
mohammadt <mohmmadt1379@gmail.com>
2023-09-26 11:27:14 +0330

    botFaciliteis

--- src/app/routes/Home/components/bot-facilities/bot-facilities.component.ts
+++ src/app/routes/Home/components/bot-facilities/bot-facilities.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-bot-facilities',
   templateUrl: './bot-facilities.component.html',
-  styleUrls: ['./bot-facilities.component.scss']
+  styleUrls: ['./bot-facilities.component.scss'],
 })
 export class BotFacilitiesComponent {
-
+  testi: boolean = false;
+  enableClick() {
+    this.testi = !this.testi;
+  }
 }
