a61db247d0168ae8396e3000ba7f08ea180e5441
mohammadt <mohmmadt1379@gmail.com>
2023-11-15 10:03:48 +0330

    addCommentComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { TagsComponent } from './tags/tags.component';
 import { AcordianComponent } from './acordian/acordian.component';
 import { CommonModule } from '@angular/common';
+import { CommentsComponent } from './comments/comments.component';
 export function playerFactory(): any {
   return import('lottie-web');
 }
 
-const components = [TagsComponent, AcordianComponent];
+const components = [TagsComponent, AcordianComponent, CommentsComponent];
 @NgModule({
   imports: [CommonModule],
   exports: [components],
