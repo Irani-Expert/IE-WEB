ff9aba1eae59a4c6b3642b05d8b4ac8c47ffe5c0
omid9151 <138152934+omid9151@users.noreply.github.com>
2023-12-03 06:27:05 -0800

    offers

--- src/app/routes/Blog/module/blog.module.ts
+++ src/app/routes/Blog/module/blog.module.ts
@@ -4,12 +4,14 @@ import { LandingBlogComponent } from '../components/landing-blog/landing-blog.co
 import { BlogRoutingModule } from './blog.routing';
 import { BlogCartComponent } from '../components/blog-cart/blog-cart.component';
 import { BlogHeroComponent } from '../components/blog-hero/blog-hero.component';
+import { OffersComponent } from '../components/offers/offers.component';
+import { DragScrollModule } from 'ngx-drag-scroll';
 
 const components =[
   LandingBlogComponent,
   BlogCartComponent,
   BlogHeroComponent,
-  
+  OffersComponent,
 ];
 
 @NgModule({
@@ -17,6 +19,7 @@ const components =[
   imports: [
     CommonModule,
     BlogRoutingModule,
+    DragScrollModule,
   ]
 })
 export class BlogModule { }
