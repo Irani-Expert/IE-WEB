767c0b94ba784bb2f571a8cc6163620dc710ba5c
omid9151 <138152934+omid9151@users.noreply.github.com>
2023-12-03 02:36:56 -0800

    blog-hero

--- src/app/routes/Blog/module/blog.module.ts
+++ src/app/routes/Blog/module/blog.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LandingBlogComponent } from '../components/landing-blog/landing-blog.component';
 import { BlogRoutingModule } from './blog.routing';
 import { BlogCartComponent } from '../components/blog-cart/blog-cart.component';
+import { BlogHeroComponent } from '../components/blog-hero/blog-hero.component';
 
 const components =[
   LandingBlogComponent,
   BlogCartComponent,
+  BlogHeroComponent,
   
 ];
 
