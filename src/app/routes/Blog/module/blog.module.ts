e9efa89ca3eaad67701745e0b579248114524e6a
omid9151 <138152934+omid9151@users.noreply.github.com>
2023-12-04 01:28:54 -0800

    pagination & articles

--- src/app/routes/Blog/module/blog.module.ts
+++ src/app/routes/Blog/module/blog.module.ts
@@ -6,17 +6,23 @@ import { BlogCartComponent } from '../components/blog-cart/blog-cart.component';
 import { BlogHeroComponent } from '../components/blog-hero/blog-hero.component';
 import { OffersComponent } from '../components/offers/offers.component';
 import { DragScrollModule } from 'ngx-drag-scroll';
+import { BlogPaginationComponent } from '../components/blog-pagination/blog-pagination.component';
+import { ArticlesComponent } from '../components/articles/articles.component';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 const components =[
   LandingBlogComponent,
   BlogCartComponent,
   BlogHeroComponent,
   OffersComponent,
+  BlogPaginationComponent,
+  ArticlesComponent,
 ];
 
 @NgModule({
   declarations: [components],
   imports: [
+    NgxPaginationModule,
     CommonModule,
     BlogRoutingModule,
     DragScrollModule,
