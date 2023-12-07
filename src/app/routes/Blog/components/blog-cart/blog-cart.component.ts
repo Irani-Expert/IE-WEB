fba35b7f7f4f2b2d75abd4ccbbc2d9254e6cc144
omid9151 <138152934+omid9151@users.noreply.github.com>
2023-12-07 00:29:00 -0800

    offer responcive

--- src/app/routes/Blog/components/blog-cart/blog-cart.component.ts
+++ src/app/routes/Blog/components/blog-cart/blog-cart.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { Utils } from 'src/app/classes/utils';
 
 @Component({
   selector: 'app-blog-cart',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./blog-cart.component.scss']
 })
 export class BlogCartComponent {
+ // =======================[رسپانسیو]==========
+  
+ device: 'sm' | 'lg' = 'lg';
+ ngOnInit(){
+   this.updateDeviceValue();
 
+ }
+ 
+ @HostListener('window:resize', ['$event'])
+ onResize() {
+   this.updateDeviceValue();
+ }
+ updateDeviceValue() {
+   if (AppComponent.isBrowser.value) {
+     if (Utils.isMobileL()) {
+       this.device = 'sm';
+     } else {
+       this.device = 'lg';
+     }
+   }
+ }
 }
--- src/app/routes/Blog/components/offers/offers.component.ts
+++ src/app/routes/Blog/components/offers/offers.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { Utils } from 'src/app/classes/utils';
 
 @Component({
   selector: 'app-offers',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./offers.component.scss']
 })
 export class OffersComponent {
+  // =======================[رسپانسیو]==========
+  
+  device: 'sm' | 'lg' = 'lg';
+  ngOnInit(){
+    this.updateDeviceValue();
+
+  }
+  
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.updateDeviceValue();
+  }
+  updateDeviceValue() {
+    if (AppComponent.isBrowser.value) {
+      if (Utils.isMobileL()) {
+        this.device = 'sm';
+      } else {
+        this.device = 'lg';
+      }
+    }
+  }
 
 }
