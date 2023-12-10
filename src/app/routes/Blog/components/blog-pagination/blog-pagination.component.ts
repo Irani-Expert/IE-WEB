cdd64dee49c0d7202a30f865c9950483285ef3b6
omid9151 <138152934+omid9151@users.noreply.github.com>
2023-12-10 06:47:43 -0800

    routers(blogs)

--- src/app/routes/Blog/components/blog-pagination/blog-pagination.component.ts
+++ src/app/routes/Blog/components/blog-pagination/blog-pagination.component.ts
@@ -39,4 +39,38 @@ export class BlogPaginationComponent {
       }
     }
   }
+    // =================[فیلتر]=============
+    category : Array<any> = [
+      {
+        name:'جدیدترین مطالب',
+        icon :'assets/img/icon-filter(blog)-1.svg',
+      },
+      {
+        name:' آموزش ها',
+        icon :'assets/img/icon-filter(blog)-7.svg',
+      },
+      {
+        name:'معرفی کتاب ',
+        icon :'assets/img/icon-filter(blog)-2.svg',
+      },
+      {
+        name:' معرفی بروکرها',
+        icon :'assets/img/icon-filter(blog)-3.svg',
+      },
+      {
+        name:' پادکست های ایرانی اکسپرت',
+        icon :'assets/img/icon-filter(blog)-4.svg',
+      },
+      {
+        name:'فارکس ',
+        icon :'assets/img/icon-filter(blog)-5.svg',
+      },
+      {
+        name:'ارز دیجیتال ',
+        icon :'assets/img/icon-filter(blog)-5.svg',
+      }, {
+        name:'ربات معامله گر ',
+        icon :'assets/img/icon-filter(blog)-6.svg',
+      },
+    ]
 }
--- src/app/routes/Blog/module/blog.routing.ts
+++ src/app/routes/Blog/module/blog.routing.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingBlogComponent } from '../components/landing-blog/landing-blog.component';
+import { BlogPaginationComponent } from '../components/blog-pagination/blog-pagination.component';
+import { LandingBlogDetailComponent } from '../components/landing-blog-detail/landing-blog-detail.component';
 
 const routes: Routes = [
     { path: '', component: LandingBlogComponent },
+    {path: 'blogs', component: BlogPaginationComponent},
+    {path:'blogs/:title', redirectTo:':title',pathMatch:'full'},
+    {path: ':title', component: LandingBlogDetailComponent }
   ];
 
   @NgModule({
