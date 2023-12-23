ebde8497a2e30a0e3e90e65bec6fcb350f00bbff
omid9151 <138152934+omid9151@users.noreply.github.com>
2023-12-23 03:52:43 -0800

    search-route

--- src/app/routes/Search/module/search.module.ts
+++ src/app/routes/Search/module/search.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { LandingSearchComponent } from '../components/landing-search/landing-search.component';
 import { HeroSearchComponent } from '../components/hero-search/hero-search.component';
 import { ItemsSearchComponent } from '../components/items-search/items-search.component';
-import { SearchRoutingModule } from './search.routing';
 
 const components = [
   LandingSearchComponent,
@@ -16,7 +15,6 @@ const components = [
 
   imports: [
     CommonModule,
-    SearchRoutingModule
   ]
 })
 export class SearchModule { }
