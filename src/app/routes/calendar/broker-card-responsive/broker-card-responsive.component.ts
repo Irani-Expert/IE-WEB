70a99a4bc78fc9b8743df0bd8834e9d162241458
omid9151 <138152934+omid9151@users.noreply.github.com>
2024-01-19 23:18:34 -0800

    broker-card-responsive

---
+++ src/app/routes/calendar/broker-card-responsive/broker-card-responsive.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-broker-card-responsive',
+  templateUrl: './broker-card-responsive.component.html',
+  styleUrls: ['./broker-card-responsive.component.scss']
+})
+export class BrokerCardResponsiveComponent {
+  
+  borderColor : string = '#FF0000';
+
+}
