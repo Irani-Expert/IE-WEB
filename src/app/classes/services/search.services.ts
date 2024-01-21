551df833882dfdb59c5acd9047f5ea1795991a15
Amin Beygi <beygoamin@gmail.com>
2024-01-21 15:23:33 +0330

    items search fixed

--- src/app/classes/services/search.services.ts
+++ src/app/classes/services/search.services.ts
@@ -1,14 +1,41 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from 'src/app/classes/services/base.service';
 import { HttpClient } from '@angular/common/http';
-import { SearchModel } from '../interfaces/search.interface';
+import {
+  SearchArticles,
+  SearchBrokers,
+  SearchCalendar,
+  SearchModel,
+  SearchProducts,
+} from '../interfaces/search.interface';
 import { ToastrService } from 'ngx-toastr';
-
+import { BehaviorSubject, lastValueFrom, map } from 'rxjs';
+class SeachModelInit implements SearchModel {
+  products: SearchProducts[];
+  brokers: SearchBrokers[];
+  articles: SearchArticles[];
+  calendarEvents: SearchCalendar[];
+  success: true;
+  message: string;
+  itemSearch: string | null;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class searchServices extends BaseService<SearchModel> {
+  searchModelSubject = new BehaviorSubject(new SeachModelInit());
+  searchModel$ = this.searchModelSubject.asObservable();
   constructor(http: HttpClient, toastr: ToastrService) {
     super(http, toastr);
+  }
+
+  getItems(someThing: string) {
+    let res = this.get(`Search?someThing=${someThing}`).pipe(
+      map((data) => {
+        this.searchModelSubject.next(data.data!);
+        return data.success;
+      })
+    );
+    return lastValueFrom(res);
   }
 }
--- src/app/routes/Search/components/items-search/items-search.component.ts
+++ src/app/routes/Search/components/items-search/items-search.component.ts
@@ -1,99 +1,81 @@
-import { Component, Input } from '@angular/core';
-import { SearchModel } from 'src/app/classes/interfaces/search.interface';
+import { Component } from '@angular/core';
+import { searchServices } from 'src/app/classes/services/search.services';
 import { environment } from 'src/environments/environment.dev';
 
 @Component({
   selector: 'app-items-search',
   templateUrl: './items-search.component.html',
-  styleUrls: ['./items-search.component.scss']
+  styleUrls: ['./items-search.component.scss'],
 })
 export class ItemsSearchComponent {
-
-
-  @Input('data') searchModel : SearchModel;
-  allItems : any = [];
+  allItems: any = [];
   contentUrl = environment.contentUrl;
-
+  loading = true;
   // ==========[هاور]=====
-  timer:any;
-  loading: boolean = true;
-  showingItem : any;
-  ngOnInit(){
-    console.log(this.tags[0]);
-
-    this.loading = false;
+  timer: any;
+  showingItem: any;
+  ngOnInit() {
     this.toggle(0);
     this.showingItem = this.allItems;
-    this.searchModel.articles.forEach((it)=>{
-      let item = {...it, tableType: 1};
-      this.allItems.push(item);
-    });
-    this.searchModel.products.forEach((it)=>{
-      let item = {...it, tableType: 6};
-      this.allItems.push(item);
-    });
-    this.searchModel.brokers.forEach((it)=>{
-      let item = {...it, tableType: 36};
-      this.allItems.push(item);
-    });
+
     // this.searchModel.calendar.forEach((it)=>{
     //   let item = {...it, tableType: 34};
     //   this.allItems.push(item);
     // });
   }
-  changeViewItem(tableType : number){
-    if (tableType == null){
+  changeViewItem(tableType: number) {
+    if (tableType == null) {
       this.showingItem = this.allItems;
-    }
-    else{
-      this.showingItem = this.allItems.slice().filter((it : any)  => it.tableType == tableType)
+    } else {
+      this.showingItem = this.allItems
+        .slice()
+        .filter((it: any) => it.tableType == tableType);
     }
   }
 
-changesort(item:any) {
-  clearTimeout(this.timer)
-  this.allItems
-  .forEach((item : any) => item.active = false);
-  item.active = true;
-}
-toggleMenuLeave(item : any){
-   this.timer = setTimeout(()=>{
-    item.active = false;
-  }, 1000);
-}
+  changesort(item: any) {
+    clearTimeout(this.timer);
+    this.allItems.forEach((item: any) => (item.active = false));
+    item.active = true;
+  }
+  toggleMenuLeave(item: any) {
+    this.timer = setTimeout(() => {
+      item.active = false;
+    }, 1000);
+  }
 
   // ===========[تگ ها]======
-  
-  tags : Array<any> = [
+
+  tags: Array<any> = [
     {
-      name :'همه',
-      id : 1,
+      name: 'همه',
+      id: 1,
       active: false,
-      tableType : null,
+      tableType: null,
     },
     {
-      name :'مقالات',
-      id : 2,
+      name: 'مقالات',
+      id: 2,
       active: false,
-      tableType: 1
+      tableType: 1,
     },
     {
-      name :'محصولات',
-      id : 3,
+      name: 'محصولات',
+      id: 3,
       active: false,
-      tableType: 6
+      tableType: 6,
     },
     {
-      name :'بروکرها',
-      id : 4,
+      name: 'بروکرها',
+      id: 4,
       active: false,
-      tableType: 36
+      tableType: 36,
     },
     {
-      name :'رویداد اقتصادی',
-      id : 5,
+      name: 'رویداد اقتصادی',
+      id: 5,
       active: false,
-      tableType : 34
+      tableType: 34,
     },
     // {
     //   name :'پادکست ها',
@@ -102,10 +84,37 @@ toggleMenuLeave(item : any){
     // }
   ];
   // =======[اکتیو]====
-  toggle(index : number){
-    this.tags.filter(
-      (tags , i) => i !== index && tags.active
-    ).forEach(btn => btn.active = !btn.active);
+  toggle(index: number) {
+    this.tags
+      .filter((tags, i) => i !== index && tags.active)
+      .forEach((btn) => (btn.active = !btn.active));
     this.tags[index].active = true;
+  }
+  constructor(private searchService: searchServices) {
+    this.searchService.searchModel$.subscribe({
+      next: (value) => {
+        this.allItems = [];
+
+        this.loading = true;
+        value.articles.forEach((it) => {
+          let item = { ...it, tableType: 1 };
+          this.allItems.push(item);
+        });
+        value.products.forEach((it) => {
+          let item = { ...it, tableType: 6 };
+          this.allItems.push(item);
+        });
+        value.brokers.forEach((it) => {
+          let item = { ...it, tableType: 36 };
+          this.allItems.push(item);
+        });
+        value.calendarEvents.forEach((it) => {
+          let item = { ...it, tableType: 34 };
+          this.allItems.push(item);
+        });
+        this.showingItem = this.allItems;
+        this.loading = false;
+      },
+    });
   }
 }
