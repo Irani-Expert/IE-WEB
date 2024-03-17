b1cf30924743da8da7f797a6cc0491a3bd890b0b
Amin Beygi <beygoamin@gmail.com>
2024-03-17 17:26:47 +0330

    creates countries coordinates

--- src/app/routes/calendar/map-components/map-itself/map.component.ts
+++ src/app/routes/calendar/map-components/map-itself/map.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import countries from 'src/assets/custom.geo.json';
+import countries_coordiantes from 'src/assets/countries-coordinates.json';
 //@ts-ignore
 import * as L from 'leaflet';
 
@@ -21,34 +22,40 @@ export class MapComponent {
   private initMap(): void {
     this.map = new L.map('map', {
       center: [51.505, -0.09],
-      draggable : false,
+      draggable: false,
       scrollWheelZoom: false,
       zoomControl: false,
       dragging: false,
       zoomDelta: 0.25,
       zoomSnap: 0,
-      zoom:1.25,
+      zoom: 1.25,
       tap: false,
     });
-    this.map.doubleClickZoom.disable(); 
+    this.map.doubleClickZoom.disable();
     var myStyle = {
       color: '#ffffff',
       weight: 1,
       opacity: 1,
       fillOpacity: 1,
       fillColor: '#e2e2e2',
-
     };
     const customIcon = L.divIcon({
       className: 'custom-bubble',
-     
+      iconSize: [10, 10],
     });
-    L.marker([51.5, -0.09],{icon: customIcon}).addTo(this.map);
+    countries_coordiantes.forEach((it) => {
+      L.marker(
+        it.coordinates,
+
+        { icon: customIcon }
+      ).addTo(this.map);
+    });
+
     L.geoJSON(countries, {
       style: myStyle,
     })
       .bindPopup(function (layer: any) {
-        return layer.feature.properties.name;
+        return layer.feature.properties.name_fa;
       })
       .addTo(this.map);
   }
