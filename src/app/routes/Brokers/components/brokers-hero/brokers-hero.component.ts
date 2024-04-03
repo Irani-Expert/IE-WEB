3bfece91ac6c9940041c27cfef6ff2694f76afff
Amin Beygi <beygoamin@gmail.com>
2024-04-03 17:00:26 +0330

    brokers header and table align change

--- src/app/routes/Brokers/components/brokers-hero/brokers-hero.component.ts
+++ src/app/routes/Brokers/components/brokers-hero/brokers-hero.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
+import { BlogService } from 'src/app/classes/services/blog.service';
 
 @Component({
   selector: 'brokers-hero',
@@ -7,6 +8,27 @@ import { AppComponent } from 'src/app/app.component';
   styleUrls: ['./brokers-hero.component.scss'],
 })
 export class BrokersHeroComponent {
+  constructor(private _blogService: BlogService) {}
+
+  ngAfterViewInit() {
+    // this.brief = this._blogService._blog?.brief ? this._blogService._blog?.brief : ''
+    // this.title = this._blogService._blog?.title ? this._blogService._blog?.title : 'بروکر چیست؟ آشنایی کامل با مفهوم بروکر فارکس'
+  }
+
+  get title() {
+    if (this._blogService._blog?.title) {
+      return this._blogService._blog.title;
+    } else {
+      return 'بروکر چیست؟ آشنایی کامل با مفهوم بروکر فارکس';
+    }
+  }
+  get brief() {
+    if (this._blogService._blog?.brief) {
+      return this._blogService._blog?.brief;
+    } else {
+      return '';
+    }
+  }
   navTo() {
     if (AppComponent.isBrowser.value) {
       const element = document.getElementById('readMore');
