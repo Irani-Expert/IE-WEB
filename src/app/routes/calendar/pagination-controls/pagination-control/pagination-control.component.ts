05f53773d41056de62298a7bb52a61e347ff81db
Amin Beygi <beygoamin@gmail.com>
2024-04-30 16:47:31 +0330

    cal event model url

--- src/app/routes/calendar/pagination-controls/pagination-control/pagination-control.component.ts
+++ src/app/routes/calendar/pagination-controls/pagination-control/pagination-control.component.ts
@@ -2,8 +2,9 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxPaginationModule, PaginationInstance } from 'ngx-pagination';
 import { EcoCalService } from 'src/app/classes/services/eco-cal.service';
-import { CalEvent } from '../../calendar/calendar-main-page/cal-event.model';
+
 import { PageInterface } from 'src/app/classes/page.model';
+import { CalEvent } from '../../calendar-main-page/cal-event.model';
 
 @Component({
   selector: 'pagination-control',
