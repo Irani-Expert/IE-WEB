f8317a9a1cfe3302d3caf9e5a2edd1adcca96c44
omid9151 <138152934+omid9151@users.noreply.github.com>
2024-05-22 05:01:38 -0700

    wayKnowType-login (100%)

--- src/app/shared/auth/signup/signup.component.ts
+++ src/app/shared/auth/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, Validators } from '@angular/forms';
 import { InputForm, InputIcon, InputInterface } from 'src/app/classes/input';
 import {
   trigger,
@@ -40,9 +40,11 @@ export class SignupComponent {
     new EventEmitter<boolean>(false);
   loading = false;
   iconSrc = 'assets/icon/eye-off.svg';
-
+  activeError1 : boolean;
+  activeError2 : boolean;
   // <!-- ==========[نحوه آشنایی]======== -->
-
+  
+  saveType : number;
   wayKnowType : Array<any> = [
     {
       id : 0,
@@ -69,16 +71,28 @@ export class SignupComponent {
       icon : 'assets/icon/people-icon.svg',
     },
   ];
+
   dropDown : boolean;
+
   toggleDropDown(){
     this.dropDown = !this.dropDown;
   }
-
   toggle(index : number){
     this.wayKnowType.filter(
       (it , i) => i !== index && it.active)
       .forEach(it => it.active = !it.active);
-      this.wayKnowType[index].active = !this.wayKnowType[index].active;
+      this.wayKnowType[index].active = true;
+      this.saveType = index;
+      if(this.saveType == 3){
+        debugger
+        this._formcontrol['parentReferralCode'].addValidators([Validators.required,Validators.minLength(2)])
+        this._formcontrol['parentReferralCode'].updateValueAndValidity({onlySelf: true})
+      }   
+      else {
+        this._formcontrol['parentReferralCode'].removeValidators([Validators.required,Validators.minLength(2)])
+        this._formcontrol['parentReferralCode'].updateValueAndValidity({onlySelf: true})
+
+      }
   }
 
   formsControlInit: InputInterface[] = [
@@ -162,6 +176,7 @@ export class SignupComponent {
 
   constructor(private authService: AuthService) {
     this.form = new FormGroup({});
+    
     this.formMaker.inputs.forEach((item) => {
       this.form.setControl(item.name, this.formMaker.createControl(item));
     });
@@ -173,12 +188,17 @@ export class SignupComponent {
   }
   async signup() {
     this.loading = true;
+
+
+
     let formData = formDataInit;
+    formData.wayKnowType = this.saveType;
     formData.password = this._formcontrol['password'].value;
     formData.accountNumber = this._formcontrol['accountNumber'].value;
     formData.email = this._formcontrol['email'].value;
     formData.firstName = this._formcontrol['firstName'].value;
     formData.lastName = this._formcontrol['lastName'].value;
+    
     
     this._formcontrol['parentReferralCode'].value
       ? (formData.parentReferralCode =
@@ -186,28 +206,41 @@ export class SignupComponent {
       : undefined;
     formData.phoneNumber = this._formcontrol['phoneNumber'].value;
     formData.userName = formData.email;
+    
     if (await this.checkFormValidation(formData)) {
       if (await this.authService.signup(formData)) this.changeView.emit(true);
       else this.loading = false;
     } else {
       console.log('Not Valid');
+
     }
   }
   async checkFormValidation(_formData: ISignUp) {
+    
     var valid = true;
     let email = _formData.email.split('@');
+
+    if(this.saveType == null){
+      this.activeError1 = true;
+      valid = false
+      return valid
+    }
+    else{
+      this.activeError1 = false;
+    } 
 
     if (this.form.valid) {
       valid = await this.checkEmail(email);
     } else {
       this.checkEmail(email);
       this.formControls.forEach((it, i) => {
-        if (this.form.controls[it.name].invalid) {
+        if (this._formcontrol[it.name].status == 'INVALID') {
           this.showError(i);
         }
       });
       valid = false;
     }
+    
 
     this.deleteError();
     this.loading = false;
